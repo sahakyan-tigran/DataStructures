4a0f2e26b5072d0d1ad6ff815cb671b95a5ed097
sahakyan-tigran <62710477+sahakyan-tigran@users.noreply.github.com>
2020-04-03 19:59:38 +0400

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -1,56 +1,56 @@
-#include "queue.h"
+#include "Queue.h"
 
 const int MAX_SIZE = 100;
 
-queue::queue() 
-	: max_size(MAX_SIZE)
+Queue::Queue() 
+	: m_max_size(MAX_SIZE)
 	, m_front(-1)
-	,m_rear(-1)
+	, m_rear(-1)
 	, m_data(nullptr) {
-	m_data = new int[max_size] {};
+	m_data = new int[m_max_size] {};
 }
 
-queue::queue(const int* data, int size) 
-	: max_size((size > MAX_SIZE) ? size : MAX_SIZE)
-	,m_front(0)
-	,m_rear(size - 1)
-	,m_data(nullptr) {
-	m_data = new int[max_size] {};
+Queue::Queue(const int* data, int size) 
+	: m_max_size((size > MAX_SIZE) ? size : MAX_SIZE)
+	, m_front(0)
+	, m_rear(size - 1)
+	, m_data(nullptr) {
+	m_data = new int[m_max_size] {};
 	for (int i = 0; i <= m_rear; ++i) {
 		m_data[i] = data[i];
 	}
 }	// remove
 
-queue::queue(const queue& object) 
-	: max_size(object.max_size)
+Queue::Queue(const queue& object) 
+	: m_max_size(object.m_max_size)
 	, m_front(object.m_front)
-	,m_rear(object.m_rear)
-	,m_data(nullptr) {
-	m_data = new int[max_size] {};
-	for (int i = 0; i < max_size; ++i) {
+	, m_rear(object.m_rear)
+	, m_data(nullptr) {
+	m_data = new int[m_max_size] {};
+	for (int i = 0; i < m_max_size; ++i) {
 		m_data[i] = object.m_data[i];
 	}
 }	// let's talk about this during lesson
 
-queue& queue::operator=(const queue& object) {
+Queue& Queue::operator=(const queue& object) {
 	if (this != &object) {
 		delete[] m_data;
-		max_size = object.max_size;
+		m_max_size = object.m_max_size;
 		m_front = object.m_front;
 		m_rear = object.m_rear;
-		m_data = new int[max_size] {};
-		for (int i = 0; i < max_size; ++i) {
+		m_data = new int[m_max_size] {};
+		for (int i = 0; i < m_max_size; ++i) {
 			m_data[i] = object.m_data[i];
 		}
 	}
 	return *this;
 }	// let's talk about this during lesson
 
-queue::~queue() {
+Queue::~Queue() {
 	delete[] m_data;
 }
 
-int queue::size() const {
+int Queue::size() const {
 	if (m_rear == m_front) {
 		if (m_rear == -1) {
 			return 0;
@@ -61,43 +61,51 @@ int queue::size() const {
 		return m_rear - m_front + 1;
 	}
 	else {
-		return max_size - (m_front - m_rear - 1);
+		return m_max_size - (m_front - m_rear - 1);
 	}
 }	// maybe it would be better to have a member m_size
 
-void queue::push(int element) {
+int& Queue::front() {
+	if(!is_empty()) {
+	       return m_data[m_front];
+	}  
+const int& Queue::front() {
+	if(!is_empty()) {
+	       return m_data[m_front];
+	}  
+void Queue::push(int element) {
 	if (is_empty()) {
 		m_front = 0;
 		m_rear = 0;
 		m_data[0] = element;
 		return;
 	}
-	if (size() == max_size) {
-		int* tmp = new int[2 * max_size];
+	if (size() == m_max_size) {
+		int* tmp = new int[2 * m_max_size]{};
 		if (m_rear > m_front) {
-			for (int i = 0; i < max_size; ++i) {
+			for (int i = 0; i < m_max_size; ++i) {
 				tmp[i] = m_data[i];
 			}
 		}
 		else {
 			int i = 0;
-			for (int j = m_front; j < max_size; ++j, ++i) {
+			for (int j = m_front; j < m_max_size; ++j, ++i) {
 				tmp[i] = m_data[j];
 			}
 			for (int j = 0; j <= m_rear; ++j, ++i) {
 				tmp[i] = m_data[j];
 			}
 			m_front = 0;
-			m_rear = max_size - 1;
+			m_rear = m_max_size - 1;
 		}
-		max_size *= 2;
+		m_max_size *= 2;
 		delete[] m_data;
 		m_data = tmp;
 		m_data[++m_rear] = element;
 		return;
 	}
 	if (m_rear >= m_front) {
-		if (m_rear == max_size - 1) {
+		if (m_rear == m_max_size - 1) {
 			m_rear = 0;
 			m_data[m_rear] = element;
 		}
@@ -110,7 +118,7 @@ void queue::push(int element) {
 	}
 }	// function is too long
 
-void queue::pop() {
+void Queue::pop() {
 	if (is_empty()) {
 		return;
 	}
@@ -119,7 +127,7 @@ void queue::pop() {
 		m_rear = 0;
 		return;
 	}
-	if (m_front == max_size - 1) {
+	if (m_front == m_max_size - 1) {
 		m_front = 0;
 	}
 	else {
