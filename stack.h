88f16870847128603755054ab8f0ced6e5f6e4b2
sahakyan-tigran <62710477+sahakyan-tigran@users.noreply.github.com>
2020-04-03 19:42:29 +0400

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <iostream>	// unnecessary include, remove
-
 class Stack {
 public:
 	Stack();
@@ -10,18 +8,17 @@ public:
 	Stack& operator=(const Stack&);
 	~Stack();
 
-	int get_size()const { return m_top+1; }	// add spaces between operands of "+", before "const". name the function "size"
-	bool is_empty()const { return m_top < 0; }	// return m_top == -1
-	int get_top()const { return m_top; };	// remove ";", should return m_data[m_top], there must be a check if the stack is not empty
-						// name the function "top"
-						// top() shoud return value "int&" in order to change the element on top of the stack
-						// also add top() function for constant objects
+	int size() const { return m_top + 1; }	 
+	bool is_empty() const { return m_top == -1; }	// return m_top == -1
+	int& top();
+	const int& top() const;
 
 	void push(int);
 	void pop();
 
 private:
 	int m_top;
-	int max_size;	// use the same naming (maybe "m_max_size")
+	int m_max_size;	
 	int* m_data;
 };
+
