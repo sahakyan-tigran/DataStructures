6ce39ccf9be331352ba8f45126f220fa7fe8b166
Ani <kocharyan-ani@users.noreply.github.com>
2020-04-03 18:44:11 +0400

    review

--- queue.h
+++ queue.h
@@ -1,28 +1,28 @@
 #pragma once
 
-#include <iostream>
+#include <iostream>	// unnecessary include, remove
 
-class queue {
+class queue {	// let's name type names with uppercase
 public:
 	queue();
-	queue(const int*,int);
+	queue(const int*,int);	// unnecessary c-tor
 	queue(const queue&);
 	queue& operator=(const queue&);
 	~queue();
     
-	int size()const;
-	bool is_empty() const { return size()== 0; }
-	int front() const { return m_front; }
+	int size()const;	// add space befor "const"
+	bool is_empty() const { return size()== 0; }	// add space
+	int front() const { return m_front; }	// The same comment for "front" function. See top() for stack
 
 	void push(int);
 	void pop();
 
 private:
-	int max_size;
+	int max_size;	// the same comment for naming. see stack
 	int m_front;
 	int m_rear;
 	int* m_data;
-
+	// so many unnecessary lines
 
 
 
