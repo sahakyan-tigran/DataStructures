ff21dede698aa414c0bf98f26a0b9484800a60e0
Ani <kocharyan-ani@users.noreply.github.com>
2020-04-03 18:35:59 +0400

    review

--- stack.h
+++ stack.h
@@ -1,24 +1,27 @@
 #pragma once
 
-#include <iostream>
+#include <iostream>	// unnecessary include, remove
 
 class Stack {
 public:
 	Stack();
-	Stack(int, const int*);
+	Stack(int, const int*);	// unnecessary c-tor
 	Stack(const Stack&);
 	Stack& operator=(const Stack&);
 	~Stack();
 
-	int get_size()const { return m_top+1; }
-	bool is_empty()const { return m_top < 0; }
-	int get_top()const { return m_top; };
+	int get_size()const { return m_top+1; }	// add spaces between operands of "+", before "const". name the function "size"
+	bool is_empty()const { return m_top < 0; }	// return m_top == -1
+	int get_top()const { return m_top; };	// remove ";", should return m_data[m_top], there must be a check if the stack is not empty
+						// name the function "top"
+						// top() shoud return value "int&" in order to change the element on top of the stack
+						// also add top() function for constant objects
 
 	void push(int);
 	void pop();
 
 private:
 	int m_top;
-	int max_size;
+	int max_size;	// use the same naming (maybe "m_max_size")
 	int* m_data;
 };
