7665dca456f28934b83a8060f11b063095e18681
sahakyan-tigran <62710477+sahakyan-tigran@users.noreply.github.com>
2020-04-03 19:59:53 +0400

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,30 +1,24 @@
 #pragma once
 
-#include <iostream>	// unnecessary include, remove
-
-class queue {	// let's name type names with uppercase
+class Queue {	
 public:
-	queue();
-	queue(const int*,int);	// unnecessary c-tor
-	queue(const queue&);
-	queue& operator=(const queue&);
-	~queue();
+	Queue();
+	Queue(const int*,int);	// unnecessary c-tor
+	Queue(const Queue&);
+	Queue& operator=(const Queue&);
+	~Queue();
     
-	int size()const;	// add space befor "const"
-	bool is_empty() const { return size()== 0; }	// add space
-	int front() const { return m_front; }	// The same comment for "front" function. See top() for stack
+	int size() const;	
+	bool is_empty() const { return size() == 0; }	
+	int& front() { return m_front; }
+	const int& front() const;
 
 	void push(int);
 	void pop();
 
 private:
-	int max_size;	// the same comment for naming. see stack
+	int m_max_size;
 	int m_front;
 	int m_rear;
 	int* m_data;
-	// so many unnecessary lines
-
-
-
-
 };
