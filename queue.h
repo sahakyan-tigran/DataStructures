5d5cc5c0b74b3fcd360e05f393664f677818bec2
sahakyan-tigran <62710477+sahakyan-tigran@users.noreply.github.com>
2020-04-04 18:28:31 +0400

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,22 +1,25 @@
 #pragma once
 
-class Queue {	
+class Queue {
 public:
 	Queue();
-	Queue(const int*,int);	// unnecessary c-tor
 	Queue(const Queue&);
 	Queue& operator=(const Queue&);
 	~Queue();
-    
-	int size() const;	
-	bool is_empty() const { return size() == 0; }	
-	int& front() { return m_front; }
+
+	int size() const { return m_size; }
+	bool is_empty() const { return m_size == 0; }
+	int& front();
 	const int& front() const;
 
 	void push(int);
 	void pop();
 
 private:
+	void resize();
+
+private:
+	int m_size;
 	int m_max_size;
 	int m_front;
 	int m_rear;
